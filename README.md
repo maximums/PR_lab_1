ad1ee2dffa715692ee6c04c110f3a5ac7ab7483a
maximums <cdodi907@gmail.com>
2020-09-19 11:22:25 +0300

    Without global vars

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # Laborator 1
+<hr>
+
+# # Used libraries :
+# # # Requests
+# # # concurrent.futures
+<p>Collecting data from 11 routes using ThreadPoolExecutor with max_workers = 5</p>
+
--- testing.py
+++ testing.py
@@ -1,12 +1,9 @@
 import requests
 from concurrent.futures import ThreadPoolExecutor, wait, FIRST_COMPLETED
 
-base_url = 'http://localhost:5000'
-register_req = requests.get(base_url + '/register').json()
-base_request = requests.get(base_url +  register_req['link'], headers={'X-Access-Token' : register_req['access_token']}).json()
 data_resp = []
 
-def make_request(route, access_token):
+def make_request(route, access_token, base_url):
     response = requests.get(base_url + route, headers={'X-Access-Token' : access_token}).json()
     data_resp.append(response)
     next_routes = []
@@ -22,14 +19,18 @@ def main(args):
         for future in done:
             if future:
                 for link in future.result():
-                    args[0].add(args[1].submit(make_request, link, args[2]))
+                    args[0].add(args[1].submit(make_request, link, args[2], args[3]))
 
 def start_point():
+    base_url = 'http://localhost:5000'
+    register_req = requests.get(base_url + '/register').json()
+    base_request = requests.get(base_url +  register_req['link'], headers={'X-Access-Token' : register_req['access_token']}).json()
+    
     e = ThreadPoolExecutor(max_workers=12)
     futures = {
-        e.submit(make_request, link, register_req['access_token']) : link for link in base_request['link'].values()
+        e.submit(make_request, link, register_req['access_token'], base_url) : link for link in base_request['link'].values()
     }
-    return [futures, e, register_req['access_token']]
+    return [futures, e, register_req['access_token'], base_url]
 
 if __name__ == "__main__":
     main(start_point())
