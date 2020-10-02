03fd8be569526755e531a21f1b13f8004b6f2e6f
maximums <cdodi907@gmail.com>
2020-10-02 16:29:58 +0300

    first commit

--- README.md
+++ README.md
@@ -1,8 +1,12 @@
 # Laborator 1
 <hr>
 
-# # Used libraries :
-# # # Requests
-# # # concurrent.futures
-<p>Collecting data from 11 routes using ThreadPoolExecutor with max_workers = 5</p>
+## Used libraries :
+1. ### Requests
+2. ### concurrent.futures
+3. ### json
+4. ### xmltodict
+5. ### yaml
+<p>Collecting data from 11 routes using ThreadPoolExecutor with max_workers = 6</p>
+
 
--- testing.py
+++ testing.py
@@ -1,13 +1,40 @@
 import requests
 from concurrent.futures import ThreadPoolExecutor, wait, FIRST_COMPLETED
+import json, xmltodict, csv, yaml
 
 data_resp = []
 
+def csv_to_dic(data):
+    tempDec = {}
+    json_data = []
+    rows = data['data'].split("\n")
+    keys = rows.pop(0).split(",")
+    for i in range(len(rows)):
+        rows[i] = rows[i].split(",")
+    for i in range(len(rows)-1):
+        for j in range(len(keys)):
+            tempDec[keys[j]] = rows[i][j]
+        json_data.append(json.dumps(tempDec))
+    return json_data
+
+def to_common_data_type(data):
+    if 'mime_type' in data:
+        if data['mime_type'] == 'application/xml':
+            data_resp.append(json.dumps(xmltodict.parse(data['data'])))
+        elif data['mime_type'] == 'text/csv':
+            data_resp.append(csv_to_dic(data))
+        elif data['mime_type'] == 'application/x-yaml':
+            data_resp.append(json.dumps(yaml.safe_load(data['data']))) 
+    else:
+        data_resp.append(data)    
+    return
+
 def make_request(route, access_token, base_url):
     response = requests.get(base_url + route, headers={'X-Access-Token' : access_token}).json()
-    data_resp.append(response)
+    # data_resp.append(response)
+    to_common_data_type(response)
+          
     next_routes = []
-
     if 'link' in response:
         for link in response['link'].values():
             next_routes.append(link)        
@@ -25,8 +52,8 @@ def start_point():
     base_url = 'http://localhost:5000'
     register_req = requests.get(base_url + '/register').json()
     base_request = requests.get(base_url +  register_req['link'], headers={'X-Access-Token' : register_req['access_token']}).json()
-    
-    e = ThreadPoolExecutor(max_workers=12)
+
+    e = ThreadPoolExecutor(max_workers=6)
     futures = {
         e.submit(make_request, link, register_req['access_token'], base_url) : link for link in base_request['link'].values()
     }
