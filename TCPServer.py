157271ae8adaabc4c76e75888bdb99f60152879b
maximums <cdodi907@gmail.com>
2020-10-07 19:42:12 +0300

    updated tcp

--- TCPServer.py
+++ TCPServer.py
@@ -8,27 +8,24 @@ def server(addr, port):
     sock.bind((addr, port))
     sock.listen()
     with ThreadPoolExecutor(max_workers=5) as executor:
-        executor.submit(handler, sock)
-        executor.submit(handler, sock)
-        executor.submit(handler, sock)
-        executor.submit(handler, sock)
+        while True:
+            print('Waiting for a connection')
+            connection, client_address = sock.accept()
+            executor.submit(handler, connection, client_address)
     return
 
-def handler(sock):
-    while True:
-        print('Waiting for a connection')
-        connection, client_address = sock.accept()
-        try:
-            print('Connection: ', client_address)
-            while True:
-                data = connection.recv(1024)
-                if not data:
-                    break
-                data = data.decode()
-                print(data)
-                connection.sendall('new version of tcp'.encode())
-        finally:
-            connection.close()
+def handler(connection, client_address):
+    try:
+        print('Connection: ', client_address)
+        while True:
+            data = connection.recv(1024)
+            if not data:
+                break
+            data = data.decode()
+            print(data)
+            connection.sendall('new version of tcp'.encode())
+    finally:
+        connection.close()
     return
 
 if __name__ == "__main__":
